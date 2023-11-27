e626889239b75731155011d635eb8e21fe9e4383
joth0003 <joth0003@stud.kea.dk>
2023-11-27 11:50:41 +0100

    prøve77

--- src/Medlem.java
+++ src/Medlem.java
@@ -1,12 +1,26 @@
 public class Medlem {
+
+
+    @Override
+    public String toString() {
+        return "Medlem" +
+                "Navn='" + navn + '\'' +
+                ", MedlemID=" + medlemID +
+                ", Alder=" + alder +
+                ", Aktivitetsform='" + aktivitetsform + '\'' +
+                ", Restance=" + restance;
+
+    }
+    private static int nextMedlemID=1000;
+
     String navn;
     int medlemID;
     int alder;
     String aktivitetsform;
     int restance;
-    Medlem (String navn, int medlemID, int alder, String af, int restance){
+    Medlem (String navn, int alder, String af, int restance){
         this.navn=navn;
-        this.medlemID=medlemID;
+        this.medlemID=nextMedlemID++;
         this.alder=alder;
         aktivitetsform=af;
         this.restance=restance;
