ef26e20179c6109e2d468c9f8de04993c6b51e2d
joth0003 <joth0003@stud.kea.dk>
2023-11-27 11:50:49 +0100

    prøve77

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        Medlem medlem1 = new Medlem("Gabe Horn", 765, "tittysvømning",1000);
+        Medlem medlem2 = new Medlem("Sam Sung",100, "Dick stuck in blender", 1200);
+        Medlem medlem3 = new Medlem("Jeremy Updog",221,"KAN IKKE SVØMME",1800);
+        Medlem medlem4 = new Medlem("Ole Wedel", 301, "Butterfly", 0);
+        System.out.println(medlem1.medlemID + " " + medlem1.navn);
         Menu.menu();
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -8,34 +8,58 @@ public class Menu {
         System.out.println("Velkommen til Delfin Svømmehallen\n");
         System.out.println("1. Betaling");
         System.out.println("2. Medlemmer");
-        System.out.println("3. Svømmer");
+        System.out.println("3. Svømmer\n");
 
         System.out.print("indtast tal:");
 
         int hovedeMenu = tastaturMenu.nextInt();
 
         switch (hovedeMenu) {
-            case 1:
+            case 1 -> {
                 System.out.println("\n1. Se restance");
-                System.out.println("2. Betal");
+                System.out.println("2. Betal\n");
                 System.out.print("indtast tal:");
                 hovedeMenu = tastaturMenu.nextInt();
-                break;
-            case 2:
-                System.out.println("\n1. Se oversigt");
+                switch (hovedeMenu){
+                    case 1:
+                        //Se restance metode//
+                    case 2:
+                        //Betal metode//
+                        hovedeMenu = tastaturMenu.nextInt();
+                }
+            }
+            case 2 -> {
+                System.out.println("\n1. Se meldemmer");
                 System.out.println("2. Opret medlem");
-                System.out.println("3. Slet medlem");
+                System.out.println("3. Slet medlem\n");
                 System.out.print("indtast tal:");
                 hovedeMenu = tastaturMenu.nextInt();
-                break;
-            case 3:
+
+                switch(hovedeMenu){
+                    case 1:
+                        System.out.println("Medlem 1: " );
+                    case 2: //opret medlem metode//
+                    case 3: //Slet medlem metode//
+                        hovedeMenu = tastaturMenu.nextInt();
+                }
+            }
+            case 3 -> {
                 System.out.println("\n1. Se oversigt over alle");
                 System.out.println("2. Top 5 svømmere");
                 System.out.println("3. Opret svømmer");
-                System.out.println("4. Slet svømmer");
+                System.out.println("4. Slet svømmer\n");
                 System.out.print("indtast tal:");
                 hovedeMenu = tastaturMenu.nextInt();
-                break;
+                switch (hovedeMenu){
+
+                    case 1:
+                        //Top 5 Svømmere metode//
+                    case 2:
+                        //Opret svømmer metode//
+                    case 3:
+                        //Slet svømmer metdoe//
+                }
+            }
         }
 
 
