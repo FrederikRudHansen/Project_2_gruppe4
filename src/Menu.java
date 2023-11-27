6f43a7681bb1fda2a50abc0d89e7aad25855f175
FrederikRudHansen <147395022+FrederikRudHansen@users.noreply.github.com>
2023-11-27 10:36:37 +0100

    First Commit

---
+++ src/Menu.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Menu {
+    static Scanner tastaturMenu = new Scanner(System.in);
+    public static void menu() {
+
+        System.out.println("Velkommen til Delfin Svømmehallen\n");
+        System.out.println("1. Betaling");
+        System.out.println("2. Medlemmer");
+        System.out.println("3. Svømmer");
+
+        System.out.print("indtast tal:");
+
+        int hovedeMenu = tastaturMenu.nextInt();
+
+        switch (hovedeMenu) {
+            case 1:
+                System.out.println("\n1. Se restance");
+                System.out.println("2. Betal");
+                System.out.print("indtast tal:");
+                hovedeMenu = tastaturMenu.nextInt();
+                break;
+            case 2:
+                System.out.println("\n1. Se oversigt");
+                System.out.println("2. Opret medlem");
+                System.out.println("3. Slet medlem");
+                System.out.print("indtast tal:");
+                hovedeMenu = tastaturMenu.nextInt();
+                break;
+            case 3:
+                System.out.println("\n1. Se oversigt over alle");
+                System.out.println("2. Top 5 svømmere");
+                System.out.println("3. Opret svømmer");
+                System.out.println("4. Slet svømmer");
+                System.out.print("indtast tal:");
+                hovedeMenu = tastaturMenu.nextInt();
+                break;
+        }
+
+
+    }
+}
+class MenuExc extends Exception{
+
+}
